b23ae9a0c0c4c0673d06c37d562111d5614b75b2
Michael Standen <ScreamingHawk@gmail.com>
2018-05-04 09:32:44 +1200

    Add web view

--- app/src/main/java/link/standen/michael/website/MainActivity.kt
+++ app/src/main/java/link/standen/michael/website/MainActivity.kt
@@ -1,12 +1,27 @@
 package link.standen.michael.website
 
+import android.annotation.SuppressLint
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Window
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+	companion object {
+		private const val WEBSITE = "https://michael.standen.link"
+	}
+
+	@SuppressLint("SetJavaScriptEnabled")
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
+		supportRequestWindowFeature(Window.FEATURE_NO_TITLE)
 		setContentView(R.layout.activity_main)
+
+		web_view.settings.javaScriptEnabled = true
+		web_view.isVerticalScrollBarEnabled = true
+		web_view.isHorizontalScrollBarEnabled = true
+
+		web_view.loadUrl(WEBSITE)
 	}
 }
